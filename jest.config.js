50e857b5b75c2569a76bf233a1739a22449a87be
Rayanne Moraes <moraesrayanne@hotmail.com>
2023-05-02 21:29:37 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct URL

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
-/* eslint-disable semi */
-/* eslint-disable comma-dangle */
-/* eslint-disable quotes */
 module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.{ts, tsx}"],
